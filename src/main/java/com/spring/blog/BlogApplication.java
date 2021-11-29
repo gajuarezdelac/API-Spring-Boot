3239aa304cdc4b8b35f435fd980d9440a07b5e5a
Gabriel Juarez De La Cruz <42722962+gajuarezdelac@users.noreply.github.com>
2021-11-29 17:46:20 -0600

    Config Deployment
    Configuración para poder realizar la configuración.

--- src/main/java/com/spring/blog/BlogApplication.java
+++ src/main/java/com/spring/blog/BlogApplication.java
@@ -1,13 +1,36 @@
 package com.spring.blog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BlogApplication {
+import com.spring.blog.domain.Role;
+import com.spring.blog.repository.RoleRepository;
 
+@SpringBootApplication
+public class BlogApplication implements CommandLineRunner{
+
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+		
+		Role userRole = new Role();
+		userRole.setName("ROLE_ADMIN");
+		roleRepository.save(userRole);
+	
+	}
+	
+	
 
 }
